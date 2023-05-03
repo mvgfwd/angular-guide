dcb5e80c6bc545bf663da67e64014d9659c0e658
yonathans <jonajr05@gmail.com>
2023-05-03 16:17:03 +0700

    course 40

--- src/app/coba/coba.component.ts
+++ src/app/coba/coba.component.ts
@@ -5,14 +5,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-coba', //target DOM
   templateUrl: './coba.component.html', //apa yang mau didisplay
+  styles: [`
+  .online{
+    color:white;
+  }`]
 })
 
-//exporting component ini
+//exporting component ini beserta function isinya
 export class CobaComponent {
   cobaNomor: number = 164;
-  statusCoba: string = 'offline';
+  statusCoba: string = Math.random() > 0.5? 'online' : 'offline';
 
   getStatusCoba() {
     return this.statusCoba;
   }
+
+  tentukanWarna(){
+    return this.statusCoba === 'online' ? 'green':'purple';
+  }
 }
+ 
--- src/app/cobacoba/cobacoba.component.ts
+++ src/app/cobacoba/cobacoba.component.ts
@@ -1,6 +1,6 @@
 //created with: ng g c cobacoba
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cobacoba', //jika element html
@@ -9,4 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './cobacoba.component.html',
   styleUrls: ['./cobacoba.component.css'],
 })
-export class CobacobaComponent {}
+export class CobacobaComponent {
+  izinkanCoba = false;
+  cobaStatus = 'Coba Binding BELUM aktif' //event binding (click)
+  twoWayBind = 'Two Way Data Binding';
+  outputNama = false;
+  
+  constructor(){
+    setTimeout(() => {
+      this.izinkanCoba=true;
+    }, 2000);
+  }
+ 
+  
+  onCreateCoba(){
+    this.cobaStatus='Coba AKTIF! Nama:'+ this.twoWayBind
+  }
+  
+  twoWayBindFunc(event:any){
+    // this.twoWayBind = event.target.value //BISA JUGA
+    this.twoWayBind = (<HTMLInputElement>event.target).value
+  }
+  
+  usernameExerc=''
+  resetUsername(){
+    this.usernameExerc=''
+  }
+
+  namaCoba(){
+    this.outputNama=true;
+  }
+
+}
